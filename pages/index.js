ca15e0e4d15bab269a61c66d1da3224b34ee27a8
Katelyn Kunzmann <katelynkunzmann@katelyns-mbp.mynetworksettings.com>
2024-01-05 22:07:20 -0500

    init of new portfolio site

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="purple" p={3} mb={6} align="center"> Hello, I'm a full stack developer </Box>
+
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Katelyn Kunzmann
+                    </Heading>
+                    <p>Coffee and Cat lover</p>
+                </Box>
+            </Box>
+        </Container>
+
+    )
+}
+
+export default Page 
