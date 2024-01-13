7e4b6b35160426df84ce05cb4c165e274891bab5
Katelyn Kunzmann <katelynk44@gmail.com>
2024-01-13 17:36:29 -0500

    Add navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 import Logo from './logo'
-import NextLink from 'next/link'
 import {
+    Text,
     Container,
     Box,
     Link,
@@ -16,25 +16,20 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 
-const LinkItem = ({ href, path, children }) => {
-    const active = path == href
-    const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+const LinkItem = ({ children, to = "/", ...rest }) => {
     return (
-        <NextLink href={href}>
-            <Link
-                p={2}
-                bg={active ? 'glassTeal' : undefined}
-                color={active ? '#202023' : inactiveColor}>
+        <Link href={to}>
+            <Text display="block" {...rest}>
                 {children}
-            </Link>
-        </NextLink>
+            </Text>
+        </Link>
     )
 }
 
 const Navbar = props => {
     const { path } = props
     return (
-        <Box
+        <Flex
             position="fixed"
             as="nav"
             w="100%"
@@ -55,8 +50,38 @@ const Navbar = props => {
                         <Logo />
                     </Heading>
                 </Flex>
+
+                <Stack
+                    direction={{ base: 'column', md: 'row' }} display={{ base: 'none', md: 'flex' }}
+                    width={{ base: 'full', md: 'auto' }}
+                    alignItems="center"
+                    flexGrow={1}
+                    mt={{ base: 4, nmd: 0 }}
+                >
+                    <LinkItem to="/about">About</LinkItem>
+                    <LinkItem to="/experience">Experience</LinkItem>
+                    <LinkItem to="/blog">Blog</LinkItem>
+                    <LinkItem to="/ventures">Ventures</LinkItem>
+                </Stack>
+
+                <Box flex={1} align="right" ml={2} display={{ base: 'inline-block', md: 'none' }}>
+                    <Menu>
+                        <MenuButton
+                            as={IconButton}
+                            aria-label='Options'
+                            icon={<HamburgerIcon />}
+                            variant='outline'
+                        />
+                        <MenuList>
+                            <MenuItem as='a' href="/about">About</MenuItem>
+                            <MenuItem as='a' href="/experience">Experience</MenuItem>
+                            <MenuItem as='a' href="/blog">Blog</MenuItem>
+                            <MenuItem as='a' href="/ventures">Ventures</MenuItem>
+                        </MenuList>
+                    </Menu>
+                </Box>
             </Container>
-        </Box>
+        </Flex>
     )
 }
 export default Navbar
